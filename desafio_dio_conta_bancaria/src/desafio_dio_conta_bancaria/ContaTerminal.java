8bb1ddc85a875be3ffb05af0f1969aaae63c5654
Jurandi Junior <jurandijr20@gmail.com>
2023-02-14 21:36:13 -0300

    Desafio Concluído

--- desafio_dio_conta_bancaria/src/desafio_dio_conta_bancaria/ContaTerminal.java
+++ desafio_dio_conta_bancaria/src/desafio_dio_conta_bancaria/ContaTerminal.java
@@ -1,9 +1,39 @@
 package desafio_dio_conta_bancaria;
 
+import java.util.Scanner;
+
 public class ContaTerminal {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+		int numero;
+		String agencia;
+		String nomeCliente;
+		double saldo;
+
+		System.out.println("---------------CONTA BANCÁRIA---------------");
+		System.out.println();
+		System.out.println("Por favor, digite o número da Conta: (1234)");
+		numero = sc.nextInt();
+
+		System.out.println("Agora digite o número da Agência: (123-8)");
+		agencia = sc.next();
+
+		System.out.println("Me informe seu nome: ");
+		nomeCliente = sc.next();
+		sc.nextLine();
+
+		System.out.println("Agora me informa seu saldo atual: ");
+		saldo = sc.nextDouble();
+
+		System.out.println();
+		System.out.printf("Olá %s, obrigado por criar uma conta em "
+				+ "nosso banco, sua agência é %s, "
+				+ "conta %d e seu saldo %.2f já está disponível"
+				+ " para saque.", nomeCliente, agencia, numero, saldo);
+
+		sc.close();
 	}
 
 }
